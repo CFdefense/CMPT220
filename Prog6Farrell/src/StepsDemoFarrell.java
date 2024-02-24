484cf0eddc47d7ac9a78ff1969cb1e0a2d14b0ac
CFdefense <152815652+CFdefense@users.noreply.github.com>
2024-02-24 18:02:44 -0500

    update

--- Prog6Farrell/src/StepsDemoFarrell.java
+++ Prog6Farrell/src/StepsDemoFarrell.java
@@ -1,9 +1,126 @@
+//Christian Farrell
+//Prog 6
+//Due 02/29/24 before 9:00 AM
+//
+//Purpose: 
+//
+//Input:
+//
+//Output:
 
-public class StepsDemoFarrell {
+//Certification of Authenticity: I certify that this lab is entirely my own work
+import java.util.*;
+public class StepsDemoFarrell 
+{
+	//Declare Keyboard
+	static Scanner keyboard = new Scanner(System.in);
+	
+	public static void main(String[] args) 
+	{
+		//Declare & Initialize Variables
+		StepsFarrell defaultSteps = new StepsFarrell();
+		String input = "";
+		char userInput = ' ';
+		int responseInputInteger = 0;
+		char responseInputChar = ' ';
+		int calcArea = 0;
+		String textDescription = "";
+		
+		//Print the menu to the user and get their input, then test their input in the switch to determine which method to run, quitting if input is 'Q'
+			do
+			{
+				System.out.println("Welcome! Please Select an Option From the Below Menu!");
+				System.out.println("W : Assign the Step Width");
+				System.out.println("N : Assign the Number of Steps");
+				System.out.println("F : Assign the Fill Style");
+				System.out.println("A : Calculate the Area");
+				System.out.println("T : Text Description of the Steps");
+				System.out.println("D : Draw the Steps");
+				System.out.println("X : Draw Thick Steps");
+				System.out.println("Q : Quit");
+				userInput = keyboard.next().toUpperCase().charAt(0);
+				
+				switch(userInput)
+				{
+				//Assign the Step Width 	
+				case 'W':
+				case 'w':
+					System.out.println("What would you like to make the Step Width?");
+					responseInputInteger = keyboard.nextInt();
+					while(responseInputInteger<= 0)
+					{
+						System.out.println("Invalid Input, What would you like to make the Step Width?");
+						responseInputInteger = keyboard.nextInt();
+					}//while
+					defaultSteps.setStepWidth(responseInputInteger);
+					break;
+					
+				//Assign the Number of Steps
+				case 'N':
+				case 'n':
+					System.out.println("What would you like to make the Number of Steps?");
+					responseInputInteger = keyboard.nextInt();
+					while(responseInputInteger<= 0)
+					{
+						System.out.println("Invalid Input, What would you like to make the Number of Steps?");
+						responseInputInteger = keyboard.nextInt();
+					}//while
+					defaultSteps.setNumSteps(responseInputInteger);
+					break;
+					
+				//Assign the Fill Style
+				case 'F':
+				case 'f':
+					System.out.println("What would you like to make the Fill Style?");
+					responseInputChar = keyboard.next().toUpperCase().charAt(0);
+					while(responseInputChar == ' ')
+					{
+						System.out.println("Invalid Input, What would you like to make the Fill Style?");
+						responseInputChar = keyboard.next().toUpperCase().charAt(0);
+					}//while
+					defaultSteps.setFillStyle(responseInputChar);
+					break;
+					
+				//Calculate the Area
+				case 'A':
+				case 'a':
+					calcArea = defaultSteps.calcArea();
+					System.out.println("The Calculated Area is " + calcArea);
+					break;
+					
+				//Text Description of the Steps
+				case 'T':
+				case 't':
+					textDescription = defaultSteps.toString();
+					System.out.println(textDescription);
+					break;
+					
+				//Draw the Steps
+				case 'D':
+				case 'd':
+					defaultSteps.drawSteps();
+					break;
+					
+				//Draw Thick Steps
+				case 'X':
+				case 'x':
+					defaultSteps.drawThickSteps();
+					break;
+					
+				//Quit
+				case 'Q':
+				case 'q':
+					break;
+					
+				//Error Catch Bad Input
+				default:
+					System.out.println("Invalid Menu Input - Please Try Again\n");
+					break;
+				}//switch
+					
+			}//do
+			while(userInput != 0);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	}//main
 
-	}
-
-}
+}//StepsDemoFarrell
--- Prog6Farrell/src/StepsFarrell.java
+++ Prog6Farrell/src/StepsFarrell.java
@@ -1,4 +1,101 @@
+import java.util.*;
+public class StepsFarrell 
+{
+	//Declare Instance Variables
+	private int myStepWidth;
+	private int myNumSteps;
+	private char myFillStyle;
+	
+	//Constructor
+	public StepsFarrell(int newStepWidth, int newNumSteps, char newFillStyle)
+	{
+		//Initialize Instance Variables
+		myStepWidth = newStepWidth;
+		myNumSteps = newNumSteps;
+		myFillStyle = newFillStyle;
+	}//StepsFarrell
+	
+	//Null Constructor
+	public StepsFarrell()
+	{
+		//Initialize Instance Variables
+		myStepWidth = 2;
+		myNumSteps = 5;
+		myFillStyle = '*';
+	}//StepsFarrell
+	
+	//Method will calculate and return the number of fillStyle characters needed for the steps
+	public int calcArea()
+	{
+		int area = 0;
+		for(int step = 0; step < myNumSteps; step++)
+			area += area+ myStepWidth;	
+		return area;
+	}//calcArea
+	
+	//Method will draw the steps to the console
+	public void drawSteps()
+	{
+		//Instance Variable
+		int currentStep = 0;
+		
+		//loop for each step and print the corresponding row
+		for(int step = 0; step < myNumSteps; step++)
+		{
+			currentStep += myStepWidth;
+			System.out.println(myFillStyle * currentStep);
+		}//for
+	}//drawSteps
+	
+	//Method will draw thick steps to the console
+	public void drawThickSteps()
+	{
+		//Instance Variable
+		int currentStep = 0;
+		
+		//loop for each step and print the corresponding row
+		for(int step = 0; step<myNumSteps; step++)
+		{
+			currentStep += myStepWidth;
+			for(int i = 0; i<3; i++)
+				System.out.println(myFillStyle * currentStep);
+		}//for
+	}//drawThickSteps
+	
+	 
+	//Instance Variable Getters, Setters and toString
+	
+	//myStepWidth Getter
+	public int getStepWidth()
+	{return myStepWidth;}//getStepWidth
+	
+	//myStepWidth Setter
+	public void setStepWidth(int newStepWidth)
+	{myStepWidth = newStepWidth;}//setStepWidth
 
-public class StepsFarrell {
-
-}
+	//myNumSteps Getter
+	public int getNumSteps()
+	{return myNumSteps;}//getNumSteps
+	
+	//myNumSteps Setter
+	public void setNumSteps(int newNumSteps)
+	{myNumSteps = newNumSteps;}//setNumSteps
+	
+	//myFillStyle Getter
+	public char getFillStyle()
+	{return myFillStyle;}//getFillStyle
+	
+	//myFillStyle Setter
+	public void setFillStyle(char newFillStyle)
+	{myFillStyle = newFillStyle;}//setFillStyle
+	
+	//toString
+	public String toString()
+	{
+		String summaryString = "My Step Width is " + myStepWidth + "\n";
+		summaryString += "My Number of Steps is " + myNumSteps + "\n";
+		summaryString += "My Fill Style is '" + myFillStyle + "' \n";
+		return summaryString;
+	}//toString
+	
+}//StepsFarrell
